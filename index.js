030db973207a36be73a7ea6bd1b4b49daf322ef3
DavidOrtegaCode <ldortegar@utn.edu.ec>
2021-11-06 17:37:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,17 +11,34 @@ server.listen(port)
 
 console.log("http server listening on %d", port)
 
-var wss = new WebSocketServer({server: server})
+var wss = new WebSocketServer({ server: server })
 console.log("websocket server created")
 
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
+wss.on("connection", function (ws) {
+  var id = setInterval(function () {
+    ws.send(JSON.stringify(new Date()), function () { })
   }, 1000)
-
   console.log("websocket connection open")
+  ws.send('welcome new cliente')
 
-  ws.on("close", function() {
+  ws.on('message', (message, isBinary) => {
+    console.log('received: ' + message)
+    // ws.send('got your message: ' + message)
+
+    //all clientes
+    wss.clients.forEach((client) => {
+      client.send(message, { binary: isBinary });
+    });
+
+    //all clientes, excluding sender.
+    // wss.clients.forEach((client) => {
+    //     if (client !== ws && client.readyState === WebSocket.OPEN) {
+    //         client.send(data, { binary: isBinary });
+    //     }
+    // });
+  })
+
+  ws.on("close", function () {
     console.log("websocket connection close")
     clearInterval(id)
   })
